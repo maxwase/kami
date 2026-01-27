baf5490f865c4280725bab90d5d57fdb0c81fbeb
Max Wase <max.vvase@gmail.com>
2026-01-27 23:37:07 +0200

    Fix lint

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ import tseslint from "typescript-eslint";
 
 export default [
   {
-    ignores: ["dist", "node_modules"],
+    ignores: ["build", "dist", "node_modules"],
   },
   js.configs.recommended,
   ...tseslint.configs.recommended,
